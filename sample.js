b3fdbcebf78c21aeead565746747ae9e2e8fc6e4
Amane Uehara <62415046+amane-uehara@users.noreply.github.com>
2022-07-24 18:00:08 +0900

    first commit

---
+++ sample.js
@@ -0,0 +1,7 @@
+window.onload = function() {
+  this.mainCanvas = document.getElementById("main-canvas");
+  this.mainCtx = this.mainCanvas.getContext("2d");
+  this.mainCanvas.width  = 100;
+  this.mainCanvas.height = 100;
+  this.mainCanvas.style.border = "4px solid #F00";
+}
